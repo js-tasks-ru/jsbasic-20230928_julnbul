d86d203a035850a04067ccb6e4ca754c56be3212
Julia Bulatova <julnbul@gmail.com>
2023-10-02 20:47:21 +0300

    factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let rezult = n;
+  if (n === 0 || n === 1) {
+    rezult = 1;
+  } else {
+    while (n > 1) {
+      n--;
+      rezult *= n;
+    }
+  }
+  return rezult;
 }
