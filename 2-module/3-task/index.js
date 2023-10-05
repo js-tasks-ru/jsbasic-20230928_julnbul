82de3d713a67b0bc6bfd0a492ac7c4b19426f8c9
Julia Bulatova <julnbul@gmail.com>
2023-10-05 16:38:32 +0300

    add calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(x, y) {
+    this.newValue = x;
+    this.newValue2 = y;
+  },
+  sum() {
+    return this.newValue + this.newValue2;
+  },
+  mul() {
+    return this.newValue * this.newValue2;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
