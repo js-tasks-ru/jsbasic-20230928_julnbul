8a1310b9735138f03496b10895d1a3baa5894bac
Julia Bulatova <julnbul@gmail.com>
2023-10-11 18:47:34 +0300

    add camelize + fix namify + fix filterRange

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
+//запись решения в учебнике видела, мой код выглядит по-другому, т.к. пока сложно 
+//отслеживать логику без промежуточных точек
+
 function camelize(str) {
-  // ваш код...
+    //массив слов без "-"
+    let arr = str.split("-");
+    //массив слов с заглавными
+    let newArr = arr.map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1));
+    //строка из массива
+    let newStr = newArr.join('');
+    return newStr;
 }
