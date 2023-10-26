0024f06d26cfd3c8113d6da57873953ee675dd63
Julia Bulatova <julnbul@gmail.com>
2023-10-26 10:47:04 +0300

    initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector(".carousel");
+  let carouselInner = document.querySelector(".carousel__inner");
+
+  let slideNumber = 0;
+  let arrowRight = document.querySelector(".carousel__arrow_right");
+  let arrowLeft = document.querySelector(".carousel__arrow_left");
+
+  if (slideNumber == 0) {
+    arrowLeft.style.display = "none";
+  }
+
+  carousel.addEventListener("click", function (event) {
+    let arrow = event.target.closest(".carousel__arrow"); 
+    let widthSlide = carouselInner.offsetWidth; 
+
+    if (arrow.classList.contains("carousel__arrow_right")) {
+      slideNumber++;
+
+      if (slideNumber == 1) {
+        arrowLeft.style.display = "";
+      } else if (slideNumber == 3) {
+        arrowRight.style.display = "none";
+      }
+
+      carouselInner.style.transform = `translateX(-${slideNumber * widthSlide}px)`; 
+    } else if (arrow.classList.contains("carousel__arrow_left")) {
+      slideNumber--;
+
+      if (slideNumber == 2) {
+        arrowRight.style.display = "";
+      } else if (slideNumber == 0) {
+        arrowLeft.style.display = "none";
+      }
+      carouselInner.style.transform = `translateX(-${slideNumber * widthSlide}px)`;
+    }
+  });
 }
